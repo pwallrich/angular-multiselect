21aba985a47445609b93277aa287b4bbfb02550d
Philipp Wallrich <p.wallrich@gmail.com>
2016-03-11 18:29:58 +0100

    changed to working

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,9 @@
 angular.module('app', ['am.multiselect'])
 
 .controller('appCtrl', ['$scope', function($scope){
-    $scope.cars = [
-                    {id:1, name: 'Audi'},
-                    {id:2, name: 'BMW'},
-                    {id:3, name: 'Honda'}
-                ];
-    $scope.selectedCar = [];
-
-    $scope.fruits = [
-                        {id: 1, name: 'Apple'},
-                        {id: 2, name: 'Orange'},
-                        {id: 3, name: 'Banana'}
-                    ];
-    $scope.selectedFruit = null;
-
     $scope.list = [];
-    for(var i = 0; i < 20; i++){
-        $scope.list.push({id: i, name: 'item '+ i});
+    for(var i = 0; i < 10; i++){
+        $scope.list.push(i);
     }
     $scope.selectedList = $scope.list[10];
 }]);
